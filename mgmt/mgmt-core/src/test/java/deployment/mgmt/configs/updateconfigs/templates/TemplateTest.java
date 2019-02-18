3c90f98ed322c30290ceeed8ffc8b4dfc70e617c
Alex Matorin <alx.matorin@gmail.com>
2019-02-18 16:40:57 +0300

    documentation update

--- mgmt/mgmt-core/src/test/java/deployment/mgmt/configs/updateconfigs/templates/TemplateTest.java
+++ mgmt/mgmt-core/src/test/java/deployment/mgmt/configs/updateconfigs/templates/TemplateTest.java
@@ -50,6 +50,6 @@ public class TemplateTest {
     public void testEnvProperties() {
         Template template = new Template("${env@Path}");
         String result = template.resolvePlaceholders(of(), templatePattern);
-        assertEquals(System.getenv("PATH"), result);
+        assertEquals(System.getenv("Path"), result);
     }
 }
