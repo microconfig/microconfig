c3483a0764eeb843dec5cb714c14e67577160fd6
Alex Matorin <alx.matorin@gmail.com>
2019-02-20 20:00:11 +0300

    fixed test

--- microconfig/microconfig-core/src/main/java/io/microconfig/properties/resolver/placeholder/strategies/MapResolveStrategy.java
+++ microconfig/microconfig-core/src/main/java/io/microconfig/properties/resolver/placeholder/strategies/MapResolveStrategy.java
@@ -6,9 +6,8 @@ import io.microconfig.properties.Property.Source;
 import io.microconfig.properties.resolver.placeholder.ResolveStrategy;
 import lombok.RequiredArgsConstructor;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 import static java.util.Optional.empty;
 import static java.util.Optional.ofNullable;
@@ -16,22 +15,22 @@ import static java.util.Optional.ofNullable;
 @RequiredArgsConstructor
 public class MapResolveStrategy implements ResolveStrategy {
     private final String componentName;
-    private final Map<String, ?> keyToValue;
+    private final Function<String, Object> keyToValue;
 
-    @SuppressWarnings("unchecked")
     public static ResolveStrategy systemPropertiesResolveStrategy() {
-        return new MapResolveStrategy("system", new HashMap(System.getProperties()));
+        return new MapResolveStrategy("system", System::getProperty);
     }
 
     public static ResolveStrategy envVariablesResolveStrategy() {
-        return new MapResolveStrategy("env", System.getenv());
+        return new MapResolveStrategy("env", System::getenv);
     }
 
     @Override
     public Optional<Property> resolve(String key, Component component, String environment) {
         if (!componentName.equals(component.getName())) return empty();
 
-        return ofNullable(keyToValue.get(key))
-                .map(value -> new Property(key, value.toString(), environment, new Source(component, componentName)));
+        return ofNullable(keyToValue.apply(key))
+                .map(Object::toString)
+                .map(value -> new Property(key, value, environment, new Source(component, componentName)));
     }
 }
