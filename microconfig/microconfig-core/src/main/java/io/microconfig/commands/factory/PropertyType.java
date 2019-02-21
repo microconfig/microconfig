ac2ab656fab38bcfe1323777924d0623b3dbd341
Alex Matorin <alx.matorin@gmail.com>
2019-02-21 12:51:34 +0300

    yaml format support

--- microconfig/microconfig-core/src/main/java/io/microconfig/commands/factory/PropertyType.java
+++ microconfig/microconfig-core/src/main/java/io/microconfig/commands/factory/PropertyType.java
@@ -22,6 +22,6 @@ public enum PropertyType {
 
     PropertyType(String configExtension, String resultFileName) {
         this.configExtension = "." + configExtension;
-        this.resultFileName = resultFileName + "." + System.getProperty("outputFormat", "yaml");
+        this.resultFileName = resultFileName + "." + System.getProperty("outputFormat", "properties");
     }
 }
