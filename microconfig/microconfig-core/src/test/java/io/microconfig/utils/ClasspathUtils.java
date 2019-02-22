5802deb600bb2ae8d855ec3f54889bdfa63f7e5e
Alex Matorin <alx.matorin@gmail.com>
2019-02-22 21:45:44 +0300

    rewrited for java 8

--- microconfig/microconfig-core/src/test/java/io/microconfig/utils/ClasspathUtils.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/utils/ClasspathUtils.java
@@ -7,7 +7,7 @@ import static java.util.Objects.requireNonNull;
 
 public class ClasspathUtils {
     public static File getClasspathFile(String name) {
-        URL url = requireNonNull(MicronconfigTestFactory.class.getClassLoader().getResource(name), () -> "File doesnt exists: " + name);
+        URL url = requireNonNull(ClasspathUtils.class.getClassLoader().getResource(name), () -> "File doesnt exists: " + name);
         return new File(url.getFile());
     }
 }
