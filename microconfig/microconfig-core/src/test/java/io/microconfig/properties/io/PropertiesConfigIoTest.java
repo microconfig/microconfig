7d84418df1f9ca6731294bdbb696caa6008f3d0c
Alex Matorin <alx.matorin@gmail.com>
2019-02-23 12:45:42 +0300

    added support for env in yaml format

--- microconfig/microconfig-core/src/test/java/io/microconfig/properties/io/PropertiesConfigIoTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/properties/io/PropertiesConfigIoTest.java
@@ -7,6 +7,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static io.microconfig.utils.ClasspathUtils.getClasspathFile;
+import static io.microconfig.utils.FileUtils.LINE_SEPARATOR;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -20,10 +21,10 @@ class PropertiesConfigIoTest {
         expected.put("p2", "p2_v");
         expected.put("p3", "=p3_v");
         expected.put("p4", ":p4_v");
-//        expected.put("jwt.publicKey", "-----BEGIN PUBLIC KEY-----\\" + LINE_SEPARATOR +
-//                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgfjVb7pJlEdu9lDPOxmi\\" + LINE_SEPARATOR +
-//                "LwIDAQAB\\" + LINE_SEPARATOR +
-//                "-----END PUBLIC KEY-----");
+        expected.put("jwt.publicKey", "-----BEGIN PUBLIC KEY-----\\" + LINE_SEPARATOR +
+                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgfjVb7pJlEdu9lDPOxmi\\" + LINE_SEPARATOR +
+                "LwIDAQAB\\" + LINE_SEPARATOR +
+                "-----END PUBLIC KEY-----");
         expected.put("empty", "");
         expected.put("empty2", "");
 
