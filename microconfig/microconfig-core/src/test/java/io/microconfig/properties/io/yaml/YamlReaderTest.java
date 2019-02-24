bb3c6477ce2d069b64e7a03a2c6516a308a8a38d
Alex Matorin <alx.matorin@gmail.com>
2019-02-24 21:28:17 +0300

    impl yaml reader

--- microconfig/microconfig-core/src/test/java/io/microconfig/properties/io/yaml/YamlReaderTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/properties/io/yaml/YamlReaderTest.java
@@ -1,6 +1,5 @@
 package io.microconfig.properties.io.yaml;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
@@ -9,7 +8,6 @@ import java.util.Map;
 import static io.microconfig.utils.ClasspathUtils.classpathFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled
 class YamlReaderTest {
     private final YamlReader reader = new YamlReader();
 
@@ -23,7 +21,7 @@ class YamlReaderTest {
         map.put("name.name2", "");
         map.put("displayName", "dv");
 
-        assertEquals(map, reader.readAsFlatMap(classpathFile("test-props/components/yaml/simple.yaml")));
+        assertEquals(map, reader.readAsFlatMap(classpathFile("files/yaml/simple.yaml")));
     }
 
     @Test
@@ -33,16 +31,15 @@ class YamlReaderTest {
         map.put("p1.p2.p3.p4.p99", "");
         map.put("p1.p2.p3.p4.p100", "p100v");
         map.put("p1.p2.p3_2", "p3_2v");
-        map.put("p1.p2.p3_2.p4_2", "");
         map.put("p1.p2.p3_2.p4_2.p5", "p5_v");
         map.put("p1.p2.p3_2.p4_2.p6", "p6_v");
         map.put("p1.p2.p3_2.p35_2", "p3_2.p35_2_v");
         map.put("p1.p2_2", "p2_2");
-        map.put("p1.p2_2.p6", "p6v");
-        map.put("p1.p2_2.p7", "p7v");
+        map.put("p1.p6", "p6v");
+        map.put("p1.p7", "p7v");
         map.put("p1.p2_3", "p2_3v");
         map.put("p9", "p9v");
 
-        assertEquals(map, reader.readAsFlatMap(classpathFile("test-props/components/yaml/inner.yaml")));
+        assertEquals(map, reader.readAsFlatMap(classpathFile("files/yaml/inner.yaml")));
     }
 }
