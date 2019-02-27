6983a05078c8477bf208550ce89fc87f3b7c6def
Alex Matorin <alx.matorin@gmail.com>
2019-02-27 16:18:33 +0300

    refactoring

--- microconfig/microconfig-core/src/main/java/io/microconfig/commands/Command.java
+++ microconfig/microconfig-core/src/main/java/io/microconfig/commands/Command.java
@@ -1,11 +1,11 @@
 package io.microconfig.commands;
 
-import java.util.stream.Stream;
+import static java.util.stream.Stream.of;
 
 public interface Command {
     void execute(CommandContext context);
 
     static Command composite(Command... commands) {
-        return context -> Stream.of(commands).forEach(c -> c.execute(context));
+        return context -> of(commands).forEach(c -> c.execute(context));
     }
 }
