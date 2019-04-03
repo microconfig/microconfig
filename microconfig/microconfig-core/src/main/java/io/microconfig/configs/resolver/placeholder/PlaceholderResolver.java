9d2ba0efaf84c7869adca7d3a9b873590226f9c4
Alex Matorin <alx.matorin@gmail.com>
2019-04-03 18:49:02 +0300

    another config type reference

--- microconfig/microconfig-core/src/main/java/io/microconfig/configs/resolver/placeholder/PlaceholderResolver.java
+++ microconfig/microconfig-core/src/main/java/io/microconfig/configs/resolver/placeholder/PlaceholderResolver.java
@@ -84,7 +84,7 @@ public class PlaceholderResolver implements PropertyResolver {
     }
 
     private Optional<Property> tryResolveForParents(Placeholder placeholderToOverride, EnvComponent root, Set<Placeholder> orderedVisited) {
-        Optional<Property> forRoot = selectStrategy(placeholderToOverride).resolve(root.getComponent(), placeholderToOverride.getValue(), root.getEnvironment());
+        Optional<Property> forRoot = selectStrategy(placeholderToOverride).resolve(root.getComponent(), placeholderToOverride.getValue(), placeholderToOverride.getEnvironment());
         if (forRoot.isPresent()) return forRoot;
 
         for (Placeholder visited : orderedVisited) {
--- microconfig/microconfig-core/src/test/java/io/microconfig/configs/ConfigProviderTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/configs/ConfigProviderTest.java
@@ -181,7 +181,7 @@ class ConfigProviderTest {
     @Test
     void testContextChange() {
         Map<String, Property> properties = provider.getProperties(byType("contextChange"), "dev");
-        assertEquals("prod c_prod", properties.get("prop4").getValue());
+        assertEquals("prod", properties.get("prop2").getValue());
     }
 
     private void doTestAliases(String componentName, String ip) {
