72920c6514b9f8783a215174fe36685d862685cf
Alex Matorin <alx.matorin@gmail.com>
2019-04-03 16:27:45 +0300

    another config type reference

--- microconfig/microconfig-core/src/main/java/io/microconfig/configs/resolver/placeholder/PlaceholderResolver.java
+++ microconfig/microconfig-core/src/main/java/io/microconfig/configs/resolver/placeholder/PlaceholderResolver.java
@@ -12,13 +12,14 @@ import lombok.RequiredArgsConstructor;
 import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Supplier;
 import java.util.regex.Matcher;
 
+import static io.microconfig.configs.resolver.placeholder.Placeholder.parse;
 import static io.microconfig.environments.Component.byType;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.unmodifiableSet;
 import static java.util.Optional.empty;
-import static java.util.Optional.of;
 
 @RequiredArgsConstructor
 public class PlaceholderResolver implements PropertyResolver {
@@ -32,70 +33,76 @@ public class PlaceholderResolver implements PropertyResolver {
     }
 
     private String doResolve(Property sourceOfPlaceholders, EnvComponent root, Set<Placeholder> visited) {
-        StringBuilder currentPropertyValue = new StringBuilder(sourceOfPlaceholders.getValue());
+        StringBuilder resultValue = new StringBuilder(sourceOfPlaceholders.getValue());
 
         while (true) {
-            Matcher matcher = Placeholder.placeholderMatcher(currentPropertyValue);
+            Matcher matcher = Placeholder.placeholderMatcher(resultValue);
             if (!matcher.find()) break;
 
-            Placeholder placeholder = newPlaceholder(matcher.group(), sourceOfPlaceholders, root, visited);
-            Optional<Property> resolvedProperty = resolveToProperty(placeholder);
-            String resolvedValue = resolveValue(resolvedProperty, root, placeholder, visited, sourceOfPlaceholders);
+            try {
+                Placeholder placeholder = parse(matcher.group(), sourceOfPlaceholders.getEnvContext());
+                String resolvedValue = resolve(placeholder, sourceOfPlaceholders, root, visited);
 
-            currentPropertyValue.replace(matcher.start(), matcher.end(), resolvedValue);
+                resultValue.replace(matcher.start(), matcher.end(), resolvedValue);
+            } catch (RuntimeException e) {
+                throw new PropertyResolveException(matcher.group(), sourceOfPlaceholders, root, e);
+            }
         }
 
-        return currentPropertyValue.toString();
+        return resultValue.toString();
     }
 
-    private Placeholder newPlaceholder(String innerPlaceholder, Property sourceOfPlaceholder, EnvComponent root, Set<Placeholder> visited) {
-        try {
-            Placeholder placeholder = Placeholder.parse(innerPlaceholder, sourceOfPlaceholder.getEnvContext());
-            return tryOverride(placeholder, sourceOfPlaceholder, root, visited);
-        } catch (IllegalArgumentException e) {
-            throw new PropertyResolveException(innerPlaceholder, sourceOfPlaceholder, root, e);
-        }
+    private String resolve(Placeholder placeholder, Property sourceOfPlaceholder, EnvComponent root, Set<Placeholder> visited) {
+        return tryResolve(placeholder, sourceOfPlaceholder, root, visited)
+                .map(value -> doResolve(value, root, updateVisited(visited, placeholder)))
+                .orElseGet(placeholderDefaultValue(placeholder, sourceOfPlaceholder, root));
+    }
+
+    private Supplier<String> placeholderDefaultValue(Placeholder placeholder, Property sourceOfPlaceholder, EnvComponent root) {
+        return () -> placeholder.getDefaultValue()
+                .orElseThrow(() -> new PropertyResolveException(placeholder.toString(), sourceOfPlaceholder, root));
     }
 
     /**
-     * 1) if component has a placeholder to himself, the placeholder value can be overriden.
-     * Example: commons has prop java.opts = '${commons@java.opts.PermSize} ${commons@java.opts.mem}'
-     * If some component includes commons(or has placeholder to commons!!!) and overrides ${java.opts.PermSize} or ${java.opts.mem} - java.opts will be resolved with overriden values.
-     * 2) Also 'this' componentName is replaced with root component name.
+     * if component has a placeholder to itself, the placeholder value can be overridden.
+     * Example: 'commons' has the property: java.opts = '${commons@java.opts.PermSize} ${commons@java.opts.mem}'
+     * If some component includes commons (or has placeholder to commons!!!) and overrides ${java.opts.PermSize} or ${java.opts.mem} - java.opts will be resolved with overridden values.
      */
-    private Placeholder tryOverride(Placeholder placeholder, Property sourceOfPlaceholder, EnvComponent root, Set<Placeholder> visited) {
+    private Optional<Property> tryResolve(Placeholder placeholder, Property sourceOfPlaceholder, EnvComponent root, Set<Placeholder> visited) {
         boolean selfReference = placeholder.isSelfReferenced();
         if (selfReference) {
             placeholder = placeholder.changeComponent(sourceOfPlaceholder.getSource().getComponent().getName());
         }
 
-        if (selfReference || (placeholderToTheSameComponent(placeholder, sourceOfPlaceholder) && !nonOverridableKeys.contains(placeholder.getValue()))) {
-            Optional<Placeholder> overriden = tryOverrideForRoot(placeholder, root);
-            if (!overriden.isPresent()) overriden = tryOverrideForParent(placeholder, visited);
-            if (overriden.isPresent()) placeholder = overriden.get();
+        if (selfReference || canBeOverridden(placeholder, sourceOfPlaceholder)) {
+            Optional<Property> resolved = resolveStrategy.resolve(root.getComponent(), placeholder.getValue(), root.getEnvironment());
+            if (resolved.isPresent()) return resolved;
+
+            resolved = tryResolveForParents(placeholder, root, visited);
+            if (resolved.isPresent()) return resolved;
         }
 
-        return placeholder;
+        return resolveToProperty(placeholder);
     }
 
-    private boolean placeholderToTheSameComponent(Placeholder placeholder, Property sourceOfPlaceholder) {
-        return placeholder.getComponent().equals(sourceOfPlaceholder.getSource().getComponent().getType())
-                && placeholder.getEnvironment().equals(sourceOfPlaceholder.getEnvContext());
+    private boolean canBeOverridden(Placeholder placeholder, Property sourceOfPlaceholder) {
+        Supplier<Boolean> placeholderToTheSameComponent = () -> placeholder.getComponent().equals(sourceOfPlaceholder.getSource().getComponent().getType())
+                && placeholder.getEnvironment().equals(sourceOfPlaceholder.getEnvContext()
+        );
+
+        return placeholderToTheSameComponent.get() && !nonOverridableKeys.contains(placeholder.getValue());
     }
 
-    private Optional<Placeholder> tryOverrideForRoot(Placeholder placeholder, EnvComponent root) {
-        return resolveStrategy.resolve(root.getComponent(), placeholder.getValue(), root.getEnvironment()).isPresent() ?
-                of(placeholder.changeComponent(root.getComponent().getName(), root.getEnvironment()))
-                : empty();
-    }
+    private Optional<Property> tryResolveForParents(Placeholder placeholderToOverride, EnvComponent root, Set<Placeholder> orderedVisited) {
+        Optional<Property> forRoot = resolveStrategy.resolve(root.getComponent(), placeholderToOverride.getValue(), root.getEnvironment());
+        if (forRoot.isPresent()) return forRoot;
 
-    private Optional<Placeholder> tryOverrideForParent(Placeholder placeholderToOverride, Set<Placeholder> orderedVisited) {
         if (orderedVisited.isEmpty()) return empty();
 
-        for (Placeholder placeholder : orderedVisited) {
-            Component component = getComponentByName(placeholder.getComponent(), placeholder.getEnvironment());
-            Optional<Placeholder> overrider = tryOverrideForRoot(placeholderToOverride, new EnvComponent(component, placeholder.getEnvironment()));
-            if (overrider.isPresent()) return overrider;
+        for (Placeholder visited : orderedVisited) {
+            Component component = findComponent(visited.getComponent(), visited.getEnvironment());
+            Optional<Property> property = resolveStrategy.resolve(component, placeholderToOverride.getValue(), visited.getEnvironment());
+            if (property.isPresent()) return property;
         }
 
         return empty();
@@ -103,33 +110,24 @@ public class PlaceholderResolver implements PropertyResolver {
 
     //must be public for plugin
     public Optional<Property> resolveToProperty(Placeholder placeholder) {
-        Component component = getComponentByName(placeholder.getComponent(), placeholder.getEnvironment());
+        Component component = findComponent(placeholder.getComponent(), placeholder.getEnvironment());
         return resolveStrategy.resolve(component, placeholder.getValue(), placeholder.getEnvironment());
     }
 
-    private Component getComponentByName(String componentName, String env) {
+    private Component findComponent(String componentNameOrType, String env) {
         try {
             return environmentProvider.getByName(env)
-                    .getComponentByName(componentName)
-                    .orElse(byType(componentName));
+                    .getComponentByName(componentNameOrType)
+                    .orElse(byType(componentNameOrType));
         } catch (EnvironmentNotExistException e) {
-            return byType(componentName);
+            return byType(componentNameOrType);
         }
-    }
-
-    private String resolveValue(Optional<Property> property, EnvComponent root, Placeholder placeholder, Set<Placeholder> visited, Property sourceOfPlaceholders) {
-        if (property.isPresent()) {
-            return doResolve(property.get(), root, updateVisited(visited, placeholder));
-        }
-
-        return placeholder.getDefaultValue()
-                .orElseThrow(() -> new PropertyResolveException(placeholder.toString(), sourceOfPlaceholders, root));
     }
 
     private Set<Placeholder> updateVisited(Set<Placeholder> visited, Placeholder placeholder) {
         Set<Placeholder> updated = new LinkedHashSet<>(visited);
         if (!updated.add(placeholder)) {
-            throw new PropertyResolveException("Found cyclic dependencies: " + updated);
+            throw new PropertyResolveException("Found placeholder cyclic dependencies: " + updated);
         }
         return unmodifiableSet(updated);
     }
