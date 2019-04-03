74dea4a98885887989698ca1aca498606fd19fa2
Alex Matorin <alx.matorin@gmail.com>
2019-04-03 20:17:17 +0300

    another config type reference

---
+++ microconfig/microconfig-core/src/test/java/io/microconfig/configs/resolver/placeholder/StrategySelectorImplTest.java
@@ -0,0 +1,12 @@
+package io.microconfig.configs.resolver.placeholder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StrategySelectorImplTest {
+    @Test
+    void selectStrategy() {
+        //todo
+    }
+}
