937bed9c3b426dc0df405d9c5d09a9f5c47e5a52
Alex Matorin <alx.matorin@gmail.com>
2019-04-12 23:07:44 +0300

    added tests

--- microconfig/microconfig-core/src/test/java/io/microconfig/configs/PropertyTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/configs/PropertyTest.java
@@ -3,17 +3,49 @@ package io.microconfig.configs;
 import io.microconfig.configs.sources.SpecialSource;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+
+import static io.microconfig.configs.Property.*;
 import static io.microconfig.configs.sources.SpecialSource.SYSTEM_SOURCE;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyTest {
+    private SpecialSource source = new SpecialSource(null, SYSTEM_SOURCE);
+
     @Test
-    void parse() {
-        Property property = Property.parse("#var key:false", "env", new SpecialSource(null, null));
-        assertEquals("key", property.getKey());
-        assertEquals("false", property.getValue());
-        assertTrue(property.isTemp());
+    void testSimpleMethods() {
+        Property tempProperty = Property.parse("#var key:false", "env", source);
+        assertEquals("key", tempProperty.getKey());
+        assertEquals("false", tempProperty.getValue());
+        assertEquals("env", tempProperty.getEnvContext());
+        assertEquals(source, tempProperty.getSource());
+        assertTrue(tempProperty.isTemp());
+
+        assertEquals(tempProperty, tempProperty("key", "false", "env", source));
+        assertEquals("#key=false", tempProperty.toString());
+
+        Property notTemp = parse("key=false", "env", source);
+        assertEquals(notTemp, property("key", "false", "env", source));
+        assertEquals("key=false", notTemp.toString());
+    }
+
+    @Test
+    void testParseException() {
+        Function<String, Property> newProperty = value -> Property.parse(value, "env", source);
+        assertDoesNotThrow(() -> newProperty.apply("key=value"));
+        assertDoesNotThrow(() -> newProperty.apply("key:value"));
+        assertThrows(IllegalArgumentException.class, () -> newProperty.apply("key"));
+    }
+
+    @Test
+    void testUtilApi() {
+        assertEquals(-1, separatorIndex("keyValue"));
+        assertEquals(3, separatorIndex("key: Value"));
+        assertEquals(3, separatorIndex("key=Value"));
+        assertTrue(isTempProperty("#var"));
+        assertFalse(isTempProperty("#Var"));
+        assertTrue(isComment("#comment"));
+        assertFalse(isComment("comment"));
     }
 
     @Test
@@ -24,6 +56,7 @@ class PropertyTest {
     }
 
     private void testEscape(String value, String expected) {
-        assertEquals(expected, Property.tempProperty("key", value, "", new SpecialSource(null, SYSTEM_SOURCE)).escapeValue());
+        assertEquals(expected, tempProperty("key", value, "", source).escapeValue());
     }
+
 }
