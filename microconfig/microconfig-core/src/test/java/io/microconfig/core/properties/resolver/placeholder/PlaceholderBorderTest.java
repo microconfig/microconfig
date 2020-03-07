802ecaac80a22dfe8ff13ca8c8c354b707ea15b8
Alex Matorin <alx.matorin@gmail.com>
2020-03-07 14:43:42 +0300

    placeholder parsing without regex

--- microconfig/microconfig-core/src/test/java/io/microconfig/core/properties/resolver/placeholder/PlaceholderBorderTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/core/properties/resolver/placeholder/PlaceholderBorderTest.java
@@ -11,10 +11,13 @@ class PlaceholderBorderTest {
     void test() {
         doTest("hello ${rf} ${c1 ${c2 } @fsd}  ${app::component[dev]@value:${another}#{1+2}} ${}", "${app::component[dev]@value:${another}#{1+2}}");
         doTest("${app::comp${c2@v2}onent[dev]@value:${another}#{1+2}}", "${c2@v2}");
+
         doTest("${component@value:${c2@v2}}", "${component@value:${c2@v2}}");
         doTest("${component@value:${c2@v2}", "${component@value:${c2@v2}");
         doTest("${component@value:${v}", "${component@value:${v}");
         doTest("${component@value${c2@v2}}", "${c2@v2}");
+        doTest("${component@${c2@v2}}", "${c2@v2}");
+        doTest("${${c1@v1}@${c2@v2}}", "${c1@v1}");
     }
 
     private void doTest(String line, String expected) {
