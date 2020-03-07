324ad9c23ff549a93381680a52b5bb662fcfc659
Alex Matorin <alx.matorin@gmail.com>
2020-03-07 14:28:52 +0300

    placeholder parsing without regex

--- microconfig/microconfig-core/src/test/java/io/microconfig/core/properties/resolver/placeholder/PlaceholderBorderTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/core/properties/resolver/placeholder/PlaceholderBorderTest.java
@@ -11,6 +11,7 @@ class PlaceholderBorderTest {
         doTest("hello ${rf} ${c1 ${c2 } @fsd}  ${app::component[dev]@value:${another}#{1+2}} ${}", "${app::component[dev]@value:${another}#{1+2}}");
         doTest("${app::comp${c2@v2}onent[dev]@value:${another}#{1+2}}", "${c2@v2}");
         doTest("${component@value:${c2@v2}}", "${component@value:${c2@v2}}");
+        doTest("${component@value${c2@v2}}", "${c2@v2}");
     }
 
     private void doTest(String line, String expected) {
