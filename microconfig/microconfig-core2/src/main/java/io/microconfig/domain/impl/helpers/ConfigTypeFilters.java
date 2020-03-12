51f07ab4b11fc707df7c1679cfebe59aad8bb26e
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-12 19:46:50 +0300

    redesign mc api

--- microconfig/microconfig-core2/src/main/java/io/microconfig/domain/impl/helpers/ConfigTypeFilters.java
+++ microconfig/microconfig-core2/src/main/java/io/microconfig/domain/impl/helpers/ConfigTypeFilters.java
@@ -4,16 +4,15 @@ import io.microconfig.domain.ConfigType;
 import io.microconfig.domain.ConfigTypeFilter;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 
 import static io.microconfig.utils.FileUtils.getExtension;
+import static io.microconfig.utils.StreamUtils.filter;
 import static io.microconfig.utils.StreamUtils.map;
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
 
 public class ConfigTypeFilters {
     public static ConfigTypeFilter eachConfigType() {
@@ -22,7 +21,7 @@ public class ConfigTypeFilters {
 
     public static ConfigTypeFilter configTypeWithName(String... name) {
         Set<String> types = new HashSet<>(asList(name));
-        return filter(type -> types.contains(type.getType()), Arrays.toString(name));
+        return filerTypes(type -> types.contains(type.getType()), types);
     }
 
     public static ConfigTypeFilter configTypeFromExtensionOf(File file) {
@@ -30,16 +29,14 @@ public class ConfigTypeFilters {
         if (ext.isEmpty()) {
             throw new IllegalArgumentException("File " + file + " doesn't have an extension. Unable to resolve component type.");
         }
-        return filter(type -> type.getSourceExtensions().contains(ext), file.getName());
+        return filerTypes(type -> type.getSourceExtensions().contains(ext), ext);
     }
 
-    private static ConfigTypeFilter filter(Predicate<ConfigType> predicate, String typeDescription) {
+    private static ConfigTypeFilter filerTypes(Predicate<ConfigType> predicate, Object typeDescription) {
         return types -> {
-            List<ConfigType> result = types.stream()
-                    .filter(predicate)
-                    .collect(toList());
+            List<ConfigType> result = filter(types, predicate);
             if (result.isEmpty()) {
-                throw new IllegalArgumentException("Unsupported config type for '" + typeDescription + "'." +
+                throw new IllegalArgumentException("Unsupported config type '" + typeDescription + "'." +
                         " Configured types: " + map(types, ConfigType::getType));
             }
             return result;
