3fd9e0102525e92436b050e7b1b2725278b68718
Evgeni Sladkovski <evgeni.sladkovski@container-solutions.com>
2020-03-16 20:34:17 +0100

    ConfigTypeFiltersTest

--- microconfig/microconfig-core2/src/test/java/io/microconfig/domain/impl/configtypes/ConfigTypeFiltersTest.java
+++ microconfig/microconfig-core2/src/test/java/io/microconfig/domain/impl/configtypes/ConfigTypeFiltersTest.java
@@ -3,14 +3,16 @@ package io.microconfig.domain.impl.configtypes;
 import io.microconfig.domain.ConfigType;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.List;
 
-import static io.microconfig.domain.impl.configtypes.ConfigTypeFilters.configTypeWithName;
-import static io.microconfig.domain.impl.configtypes.ConfigTypeFilters.eachConfigType;
-import static io.microconfig.domain.impl.configtypes.StandardConfigType.APPLICATION;
-import static io.microconfig.domain.impl.configtypes.StandardConfigType.HELM;
+import static io.microconfig.domain.impl.configtypes.ConfigTypeFilters.*;
+import static io.microconfig.domain.impl.configtypes.StandardConfigType.*;
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ConfigTypeFiltersTest {
     private final List<ConfigType> types = new StandardConfigTypeRepository().getConfigTypes();
@@ -22,8 +24,33 @@ class ConfigTypeFiltersTest {
     }
 
     @Test
+    void useProvidedTypes() {
+        List<ConfigType> selected = configType(DEPLOY).selectTypes(emptyList());
+        assertEquals(singletonList(DEPLOY), selected);
+    }
+
+    @Test
     void selectByConfigName() {
         List<ConfigType> selected = configTypeWithName("app", "helm").selectTypes(types);
         assertEquals(asList(APPLICATION, HELM), selected);
     }
+
+    @Test
+    void failOnUnsupportedName() {
+        assertThrows(IllegalArgumentException.class, () -> configTypeWithName("badName").selectTypes(types));
+    }
+
+    @Test
+    void selectByFileExtension() {
+        File application = new File("application.yaml");
+        List<ConfigType> selected = configTypeWithExtension(application).selectTypes(types);
+        assertEquals(singletonList(APPLICATION), selected);
+    }
+
+    @Test
+    void failOnFileWithoutExtension() {
+        File badFile = new File("application");
+        assertThrows(IllegalArgumentException.class, () -> configTypeWithExtension(badFile).selectTypes(types));
+    }
+
 }
