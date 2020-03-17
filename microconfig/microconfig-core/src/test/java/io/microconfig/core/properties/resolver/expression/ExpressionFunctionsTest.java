4d16dfe52a4398e56f61e84c57b791e53d4e6b9c
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-17 15:12:28 +0300

    #7 support for predefined functions

--- microconfig/microconfig-core/src/test/java/io/microconfig/core/properties/resolver/expression/ExpressionFunctionsTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/core/properties/resolver/expression/ExpressionFunctionsTest.java
@@ -3,13 +3,15 @@ package io.microconfig.core.properties.resolver.expression;
 import org.junit.jupiter.api.Test;
 
 import static io.microconfig.core.properties.resolver.expression.ExpressionFunctions.findGroup;
+import static io.microconfig.core.properties.resolver.expression.ExpressionFunctions.findGroupOrDefault;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExpressionFunctionsTest {
     @Test
     void testFindGroup() {
-        assertEquals("", findGroup("xmx", "some"));
         assertEquals("100m", findGroup("xmx(?<xmx>.+)", "xmx100m"));
         assertEquals("100", findGroup("\\d+", "xmx100m"));
+        assertEquals("", findGroup("xmx", "some"));
+        assertEquals("empty", findGroupOrDefault("xmx", "some", "empty"));
     }
 }
