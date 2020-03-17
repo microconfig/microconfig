6bd83d486fb3fe1d79579b467b448de466a86370
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-17 15:10:19 +0300

    #7 support for predefined functions

--- microconfig/microconfig-core/src/main/java/io/microconfig/core/properties/resolver/expression/ExpressionFunctions.java
+++ microconfig/microconfig-core/src/main/java/io/microconfig/core/properties/resolver/expression/ExpressionFunctions.java
@@ -7,7 +7,11 @@ import static java.lang.Math.min;
 
 public class ExpressionFunctions {
     public static String findGroup(String regex, String line) {
+        return findGroupOrDefault(regex, line, "");
+    }
+
+    public static String findGroupOrDefault(String regex, String line, String defaultValue) {
         Matcher matcher = Pattern.compile(regex).matcher(line);
-        return matcher.find() ? matcher.group(min(1, matcher.groupCount())) : "";
+        return matcher.find() ? matcher.group(min(1, matcher.groupCount())) : defaultValue;
     }
 }
