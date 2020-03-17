f3583e5cd987744284898669c9c20c141c507263
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-17 21:04:40 +0300

    redesign mc api

--- microconfig/microconfig-core2/src/test/java/io/microconfig/domain/impl/properties/resolvers/placeholder/PlaceholderBordersTest.java
+++ microconfig/microconfig-core2/src/test/java/io/microconfig/domain/impl/properties/resolvers/placeholder/PlaceholderBordersTest.java
@@ -8,7 +8,7 @@ class PlaceholderBordersTest {
     @Test
     void test() {
         doTest("hello ${rf} ${c1 ${c2 } @fsd}  ${app::component[dev]@value:${another}#{1+2}} ${}", "${app::component[dev]@value:${another}#{1+2}}");
-        doTest("${app::comp${c2@v2}onent[dev]@value:${another}#{1+2}}", "${c2@v2}");
+        doTest("${comp${c2@v2}onent[dev]@value:${another}#{1+2}}", "${c2@v2}");
 
         doTest("${component@value:${c2@v2}}", "${component@value:${c2@v2}}");
         doTest("${component@value:${c2@v2}", "${component@value:${c2@v2}");
