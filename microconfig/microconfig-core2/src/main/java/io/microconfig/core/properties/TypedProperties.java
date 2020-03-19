b99a6813796225fedf1a0c598c40089bea299b00
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-19 23:21:30 +0300

    layout

--- microconfig/microconfig-core2/src/main/java/io/microconfig/core/properties/TypedProperties.java
+++ microconfig/microconfig-core2/src/main/java/io/microconfig/core/properties/TypedProperties.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.Optional;
 
 public interface TypedProperties {
+    String getComponent();
+
     String getConfigType();
 
     TypedProperties resolveBy(Resolver resolver);
