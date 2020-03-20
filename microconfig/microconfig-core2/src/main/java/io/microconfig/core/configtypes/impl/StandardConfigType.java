ff1be4066983682200693958ce254434fd06ec7c
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-20 23:16:38 +0300

    redesign mc api

--- microconfig/microconfig-core2/src/main/java/io/microconfig/core/configtypes/impl/StandardConfigType.java
+++ microconfig/microconfig-core2/src/main/java/io/microconfig/core/configtypes/impl/StandardConfigType.java
@@ -14,11 +14,15 @@ public enum StandardConfigType implements ConfigType {
     APPLICATION("app", setOf(".properties", ".yaml"), "application"),
     PROCESS("process", setOf(".process", ".proc"), "process"),
     HELM("helm", setOf(".helm"), "values"),
-    DEPLOY("deploy", setOf(".deploy"), "deploy"),
-    ENV("env", setOf(".env"), "env"),
-    SECRET("secret", setOf(".secret"), "secret"),
-    LOG4J("log4j", setOf(".log4j"), "log4j"),
-    LOG4J2("log4j2", setOf(".log4j2"), "log4j2");
+    DEPLOY("deploy"),
+    ENV("env"),
+    SECRET("secret"),
+    LOG4J("log4j"),
+    LOG4J2("log4j2");
+
+    StandardConfigType(String name) {
+        this(name, setOf("." + name), name);
+    }
 
     private final String name;
     private final Set<String> sourceExtensions;
