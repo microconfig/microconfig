2a927a880b1fe3fd02c5b83fe96af5804a7925a3
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-26 21:57:16 +0300

    redesign mc api

--- microconfig/microconfig-core/src/test/java/io/microconfig/MicroconfigTest.java
+++ microconfig/microconfig-core/src/test/java/io/microconfig/MicroconfigTest.java
@@ -73,7 +73,7 @@ public class MicroconfigTest {
         } else {
             info(red("Failed '" + component + "'. Expected/Actual:")
                     + "\n" + e
-                    + "'\n***\n" + a +"'"
+                    + "\n***\n" + a
             );
         }
         return result;
