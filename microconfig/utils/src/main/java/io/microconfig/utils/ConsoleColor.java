3e661457df3c1f2c61669ce3656b890823990a89
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-26 18:49:05 +0300

    redesign mc api

--- microconfig/utils/src/main/java/io/microconfig/utils/ConsoleColor.java
+++ microconfig/utils/src/main/java/io/microconfig/utils/ConsoleColor.java
@@ -1,10 +1,10 @@
 package io.microconfig.utils;
 
 public class ConsoleColor {
-    private static final String RESET = "\u001B[0m";
-    private static final String RED = "\u001B[31m";
     private static final String GREEN = "\u001B[32m";
+    private static final String RED = "\u001B[31m";
     private static final String YELLOW = "\u001B[33m";
+    private static final String RESET = "\u001B[0m";
 
     public static String green(String message) {
         return applyColor(message, GREEN);
