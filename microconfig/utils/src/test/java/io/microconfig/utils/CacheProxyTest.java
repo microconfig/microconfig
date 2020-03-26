c9baaf03a94e52700c94d3ab66af53159cb66add
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-26 18:29:23 +0300

    redesign mc api

--- microconfig/utils/src/test/java/io/microconfig/utils/CacheProxyTest.java
+++ microconfig/utils/src/test/java/io/microconfig/utils/CacheProxyTest.java
@@ -2,11 +2,14 @@ package io.microconfig.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiFunction;
 
 import static io.microconfig.utils.CacheProxy.cache;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CacheProxyTest {
     @Test
@@ -20,5 +23,27 @@ class CacheProxyTest {
         assertEquals(3, cached.apply(2, "3"));
         assertEquals(2, cached.apply(2, "2"));
         assertEquals(1, cached.apply(1, "2"));
+    }
+
+    @Test
+    void testThrows() {
+        {
+            Runnable r = () -> {
+                throw new IllegalStateException();
+            };
+            assertThrows(IllegalStateException.class, () -> cache(r).run());
+        }
+        {
+            Runnable r = () -> {
+                throw new Error();
+            };
+            assertThrows(Error.class, () -> cache(r).run());
+        }
+        {
+            Callable<?> r = () -> {
+                throw new IOException();
+            };
+            assertThrows(RuntimeException.class, () -> cache(r).call());
+        }
     }
 }
