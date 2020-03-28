a0130aa1ef5e496be873a28cf9eace0fedebac8d
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-03-28 17:17:00 +0300

    redesign mc api

---
+++ microconfig-core/src/test/java/io/microconfig/core/properties/repository/ComponentNotFoundExceptionTest.java
@@ -0,0 +1,21 @@
+package io.microconfig.core.properties.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ComponentNotFoundExceptionTest {
+    @Test
+    void test() {
+        ComponentNotFoundException exception = new ComponentNotFoundException("p4");
+        exception.withParentComponent("p3");
+        exception.withParentComponent("p2");
+        exception.withParentComponent("p1");
+
+        try {
+            throw exception;
+        } catch (ComponentNotFoundException e) {
+            assertEquals("Component 'p4' doesn't exist. Dependency chain: p1 -> p2 -> p3", e.getMessage());
+        }
+    }
+}
