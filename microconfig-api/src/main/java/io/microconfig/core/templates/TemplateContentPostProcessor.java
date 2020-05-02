5f105d8611205697645144672e3bb31a8623c2e9
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-05-02 23:04:28 +0300

    #18 mustache support

--- microconfig-api/src/main/java/io/microconfig/core/templates/TemplateContentPostProcessor.java
+++ microconfig-api/src/main/java/io/microconfig/core/templates/TemplateContentPostProcessor.java
@@ -6,8 +6,4 @@ import java.io.File;
 
 public interface TemplateContentPostProcessor {
     String process(String templateName, File source, String content, TypedProperties properties);
-
-    static TemplateContentPostProcessor empty() {
-        return (templateName, _1, content, _3) -> content;
-    }
 }
