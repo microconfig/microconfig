d0156237051ffb9f965095e752ba4f6630b08523
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-05-03 00:29:53 +0300

    #18 mustache support

--- microconfig-core/src/main/java/io/microconfig/core/properties/templates/MustacheTemplateProcessor.java
+++ microconfig-core/src/main/java/io/microconfig/core/properties/templates/MustacheTemplateProcessor.java
@@ -1,15 +1,13 @@
 package io.microconfig.core.properties.templates;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
+import com.samskivert.mustache.Mustache;
+import com.samskivert.mustache.Template;
 import io.microconfig.core.properties.TypedProperties;
 import io.microconfig.core.properties.io.yaml.YamlTreeImpl;
 import io.microconfig.core.templates.TemplateContentPostProcessor;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Map;
 
 import static io.microconfig.utils.Logger.info;
@@ -22,18 +20,15 @@ public class MustacheTemplateProcessor implements TemplateContentPostProcessor {
         if (!isMustacheTemplate(source, templateName)) return content;
 
         info("Using mustache template for " + properties.getDeclaringComponent().getComponent() + "/" + source.getName());
-        return compile(content)
-                .execute(new StringWriter(), toYaml(properties))
-                .toString();
+        return compile(content).execute(toYaml(properties));
     }
 
     private boolean isMustacheTemplate(File source, String templateName) {
         return source.getName().endsWith("." + MUSTACHE) || templateName.contains(MUSTACHE);
     }
 
-    private Mustache compile(String source) {
-        return new DefaultMustacheFactory()
-                .compile(new StringReader(source), "");
+    private Template compile(String source) {
+        return Mustache.compiler().compile(source);
     }
 
     private Map<String, Object> toYaml(TypedProperties properties) {
