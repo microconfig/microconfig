bed868e3d3d2633cff2d92ae137214cb7c9ebcc0
Evgeni Sladkowski <kapodes@gmail.com>
2020-05-03 23:22:46 +0200

    native image

--- agent.sh
+++ agent.sh
@@ -1,3 +1,3 @@
 ~/graalvm/bin/java -agentlib:native-image-agent=config-output-dir=native \
   -jar microconfig-cli/build/libs/microconfig-cli-4.1.1-all.jar \
-  -r /Users/kapodes/IdeaProjects/microconfig/microconfig-core/src/test/resources/templates/ -d /Users/kapodes/microconfig/build -e dev
+  -r /Users/kapodes/IdeaProjects/microconfig-quickstart -d /Users/kapodes/IdeaProjects/microconfig-quickstart/build -e dev
