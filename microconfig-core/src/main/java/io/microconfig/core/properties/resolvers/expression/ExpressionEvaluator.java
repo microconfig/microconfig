161f78df463817d34d029bd9365be9a8501a1d91
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-05-05 15:04:41 +0300

    plugin api

--- microconfig-core/src/main/java/io/microconfig/core/properties/resolvers/expression/ExpressionEvaluator.java
+++ microconfig-core/src/main/java/io/microconfig/core/properties/resolvers/expression/ExpressionEvaluator.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 import static java.lang.reflect.Modifier.isStatic;
 import static java.util.stream.Stream.of;
-import static org.springframework.expression.spel.support.SimpleEvaluationContext.forReadOnlyDataBinding;
 
 @RequiredArgsConstructor
 class ExpressionEvaluator {
@@ -15,9 +15,10 @@ class ExpressionEvaluator {
     private final EvaluationContext context;
 
     public static ExpressionEvaluator withFunctionsFrom(Class<?>... functionClasses) {
-        EvaluationContext context = forReadOnlyDataBinding()
-                .withInstanceMethods()
-                .build();
+//        EvaluationContext context = forReadOnlyDataBinding()
+//                .withInstanceMethods()
+//                .build();
+        EvaluationContext context = new StandardEvaluationContext();
 
         of(functionClasses)
                 .flatMap(c -> of(c.getMethods()))
