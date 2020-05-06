a02b189a1df2c03d18d9c0d65dbf514f79ad8878
Evgeni Sladkowski <kapodes@gmail.com>
2020-05-06 21:30:06 +0200

    native image action

--- graalvm-mac.sh
+++ graalvm-mac.sh
@@ -1,9 +1,9 @@
-curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.0.0/graalvm-ce-java11-darwin-amd64-20.0.0.tar.gz -o graalvm.tar.gz
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.0.0/graalvm-ce-java8-darwin-amd64-20.0.0.tar.gz -o graalvm.tar.gz
 mkdir .graalvm
 tar -xf graalvm.tar.gz -C .graalvm
 rm graalvm.tar.gz
-mv .graalvm/graalvm-ce-java11-20.0.0/Contents/Home/* .graalvm/
-rm -rf .graalvm/graalvm-ce-java11-20.0.0
+mv .graalvm/graalvm-ce-java8-20.0.0/Contents/Home/* .graalvm/
+rm -rf .graalvm/graalvm-ce-java8-20.0.0
 chmod +x .graalvm/bin/gu
 .graalvm/bin/gu install -n native-image
 .graalvm/bin/native-image --version
