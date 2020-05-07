dfc9d092740792ca65e521d38d2b045155261c6c
Evgeni Sladkowski <kapodes@gmail.com>
2020-05-07 23:35:40 +0200

    native image action

---
+++ native.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+graalvm/bin/native-image.cmd -jar microconfig-cli/build/libs/microconfig-cli-*-all.jar
