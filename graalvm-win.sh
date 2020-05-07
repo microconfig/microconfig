1839fc78fe362a81d664ffb9dbe63935d79431ff
Evgeni Sladkowski <kapodes@gmail.com>
2020-05-07 19:56:57 +0200

    native image action

--- graalvm-win.sh
+++ graalvm-win.sh
@@ -1,5 +1,5 @@
 curl -sL https://github.com/graalvm/graalvm-ce-dev-builds/releases/download/20.1.0-dev-20200506_1135/graalvm-ce-java11-windows-amd64-20.1.0-dev.zip -o graalvm.zip
-unzip graalvm.zip -q -d graalvm
+unzip -q graalvm.zip -d graalvm
 rm graalvm.zip
 mv graalvm/graalvm-ce-java11-20.1.0-dev/* graalvm/
 rm -rf graalvm/graalvm-ce-java11-20.1.0-dev
