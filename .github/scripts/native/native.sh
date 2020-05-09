70c6d191704ff4a5f71f6f46b0af02cdfb5338fa
Evgeni Sladkowski <kapodes@gmail.com>
2020-05-09 15:54:24 +0200

    complex build

---
+++ .github/scripts/native/native.sh
@@ -0,0 +1,1 @@
+.graalvm/bin/native-image -jar microconfig.jar
--- native/native.sh
+++
@@ -1,7 +0,0 @@
-.graalvm/bin/native-image -jar microconfig-cli/build/libs/microconfig-cli-*-all.jar
-
-
-#    -H:+PrintClassInitialization \
-#    --allow-incomplete-classpath \
-#    --report-unsupported-elements-at-runtime \
-#  -Dorg.apache.logging.log4j.simplelog.StatusLogger.level=OFF \
