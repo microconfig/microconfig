e2142c6d377449f913ffce410d6f2796655294a5
Evgeni Sladkowski <kapodes@gmail.com>
2020-06-03 21:17:00 +0200

    official graalvm release for builds

--- .github/scripts/native/graalvm-mac.sh
+++ .github/scripts/native/graalvm-mac.sh
@@ -1,9 +1,9 @@
-curl -sL https://github.com/graalvm/graalvm-ce-dev-builds/releases/download/20.1.0-dev-20200506_1135/graalvm-ce-java11-darwin-amd64-20.1.0-dev.tar.gz -o graalvm.tar.gz
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.1.0/graalvm-ce-java11-darwin-amd64-20.1.0.tar.gz -o graalvm.tar.gz
 mkdir .graalvm
 tar -xf graalvm.tar.gz -C .graalvm
 rm graalvm.tar.gz
-mv .graalvm/graalvm-ce-java11-20.1.0-dev/Contents/Home/* .graalvm/
-rm -rf .graalvm/graalvm-ce-java11-20.1.0-dev
+mv .graalvm/graalvm-ce-java11-20.1.0/Contents/Home/* .graalvm/
+rm -rf .graalvm/graalvm-ce-java11-20.1.0
 chmod +x .graalvm/bin/gu
 .graalvm/bin/gu install -n native-image
 .graalvm/bin/native-image --version
--- .github/scripts/native/graalvm-win.sh
+++ .github/scripts/native/graalvm-win.sh
@@ -1,7 +1,7 @@
-curl -sL https://github.com/graalvm/graalvm-ce-dev-builds/releases/download/20.1.0-dev-20200506_1135/graalvm-ce-java11-windows-amd64-20.1.0-dev.zip -o graalvm.zip
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.1.0/graalvm-ce-java11-windows-amd64-20.1.0.zip -o graalvm.zip
 unzip -q graalvm.zip -d graalvm
 rm graalvm.zip
-mv graalvm/graalvm-ce-java11-20.1.0-dev/* graalvm/
-rm -rf graalvm/graalvm-ce-java11-20.1.0-dev
+mv graalvm/graalvm-ce-java11-20.1.0/* graalvm/
+rm -rf graalvm/graalvm-ce-java11-20.1.0
 graalvm/bin/gu.cmd install -n native-image
 graalvm/bin/native-image.cmd --version
