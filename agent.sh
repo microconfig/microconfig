b83150245abd4fa8a5abc52adf682bad85bb2ea4
Evgeni Sladkowski <kapodes@gmail.com>
2020-06-20 17:12:58 +0200

    native image settings update

--- agent.sh
+++ agent.sh
@@ -1,3 +1,3 @@
-~/graalvm/bin/java -agentlib:native-image-agent=config-output-dir=native \
+~/graalvm/bin/java -agentlib:native-image-agent=config-merge-dir=microconfig-cli/src/main/resources/META-INF/native-image \
   -jar microconfig-cli/build/libs/microconfig-cli-*-all.jar \
-  -r /Users/kapodes/IdeaProjects/microconfig-quickstart -d /Users/kapodes/IdeaProjects/microconfig-quickstart/build -e dev
+  -r ~/microconfig/microconfig-quickstart -d ~/microconfig/microconfig-quickstart/build -e dev $1
