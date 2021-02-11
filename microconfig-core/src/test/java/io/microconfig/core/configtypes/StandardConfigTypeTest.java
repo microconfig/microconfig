7ca1a3768e9b821dc793cb6037c8d4bbe3fe3ee2
alxmatorin <Matorin.A.Aleks@sberbank.ru>
2021-02-11 19:20:31 +0300

    added .yml as supported app config type

--- microconfig-core/src/test/java/io/microconfig/core/configtypes/StandardConfigTypeTest.java
+++ microconfig-core/src/test/java/io/microconfig/core/configtypes/StandardConfigTypeTest.java
@@ -11,7 +11,7 @@ class StandardConfigTypeTest {
     public void shouldImplementConfigType() {
         ConfigType app = APPLICATION;
         assertEquals("app", app.getName());
-        assertEquals(setOf(".yaml", ".properties"), app.getSourceExtensions());
+        assertEquals(setOf(".yml", ".yaml", ".properties"), app.getSourceExtensions());
         assertEquals("application", app.getResultFileName());
     }
 }
