869505f6942d57e2dcbad9d72c388d8b43d5cd90
Alex <Matorin.A.Aleks@sberbank.ru>
2021-08-02 21:16:49 +0300

    #45 changed priority of profiles. added testes

--- microconfig-core/src/main/java/io/microconfig/core/properties/repository/ConfigFile.java
+++ microconfig-core/src/main/java/io/microconfig/core/properties/repository/ConfigFile.java
@@ -24,7 +24,6 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.Stream.concat;
 
 @Getter
-@ToString(of = "file")
 @EqualsAndHashCode(of = "file")
 @RequiredArgsConstructor
 public class ConfigFile {
@@ -69,5 +68,10 @@ public class ConfigFile {
     private Map<String, Property> joinToMap(List<Property> properties, List<Property> tempProperties) {
         return concat(properties.stream(), tempProperties.stream())
                 .collect(toLinkedMap(Property::getKey, identity()));
+    }
+
+    @Override
+    public String toString() {
+        return file.toString();
     }
 }
