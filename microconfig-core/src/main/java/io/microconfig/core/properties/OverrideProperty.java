79946e5dd755cb0670a0c48f4df0f670af9b1073
Evgeni Sladkowski <kapodes@gmail.com>
2022-06-15 22:05:46 +0200

    changed env in file overrides syntax (#53)
    * changed env in file overrides syntax for better readability

--- microconfig-core/src/main/java/io/microconfig/core/properties/OverrideProperty.java
+++ microconfig-core/src/main/java/io/microconfig/core/properties/OverrideProperty.java
@@ -9,28 +9,27 @@ import static lombok.AccessLevel.PRIVATE;
 @RequiredArgsConstructor(access = PRIVATE)
 @EqualsAndHashCode
 public class OverrideProperty implements Property {
-    private static final String ENV_VAR_VALUE = "@var.";
+    private static final String MULTI_VAR_PREFIX = "@var.";
 
     @Getter
     private final String environment;
     private final Property delegate;
 
     public static boolean isOverrideProperty(String line) {
-        return line.startsWith("@");
+        return line.startsWith("@") || line.startsWith("+");
     }
 
     public static Property overrideProperty(String key, String value, ConfigFormat configFormat, DeclaringComponent source) {
-        boolean isVar = key.contains(ENV_VAR_VALUE);
+        boolean isVar = key.startsWith("@");
         int offset = key.indexOf('.');
-        String envName = extractEnv(key, offset, isVar);
+        String envName = extractEnv(key, offset);
         String adjustedKey = key.substring(offset + 1);
         Property delegate = new PropertyImpl(adjustedKey, value, isVar, configFormat, source);
         return new OverrideProperty(envName, delegate);
     }
 
-    private static String extractEnv(String key, int offset, boolean isVar) {
-        if (key.startsWith(ENV_VAR_VALUE)) return null;
-        return key.substring(1, isVar ? key.lastIndexOf('@') : offset);
+    private static String extractEnv(String key, int offset) {
+        return key.startsWith(MULTI_VAR_PREFIX) ? null : key.substring(1, offset);
     }
 
     @Override
