3f5f086f09b2c5e6253125ff6b299cdc72829270
Evgenii Sladkovskii <kapodes@gmail.com>
2023-01-03 17:09:00 +0100

    fix windows native build

--- .github/scripts/native/native.bat
+++ .github/scripts/native/native.bat
@@ -1,2 +1,2 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 graalvm\bin\native-image.cmd -jar microconfig.jar
