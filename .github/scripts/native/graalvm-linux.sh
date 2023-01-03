8806eda461d1d511e7616d06a8768139b3013ac0
Evgenii Sladkovskii <kapodes@gmail.com>
2023-01-03 16:21:53 +0100

    update native image scripts

--- .github/scripts/native/graalvm-linux.sh
+++ .github/scripts/native/graalvm-linux.sh
@@ -1,9 +1,9 @@
-curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.1.0/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz -o graalvm.tar.gz
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java11-linux-amd64-22.3.0.tar.gz -o graalvm.tar.gz
 mkdir .graalvm
 tar -xf graalvm.tar.gz -C .graalvm
 rm graalvm.tar.gz
-mv .graalvm/graalvm-ce-java11-20.1.0/* .graalvm/
-rm -rf .graalvm/graalvm-ce-java11-20.1.0
+mv .graalvm/graalvm-ce-java11-22.3.0/* .graalvm/
+rm -rf .graalvm/graalvm-ce-java11-22.3.0
 chmod +x .graalvm/bin/gu
 .graalvm/bin/gu install -n native-image
 .graalvm/bin/native-image --version
