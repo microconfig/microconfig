98f06f107fb921e50c1fee73b469163de019b9b5
Evgeni Sladkowski <kapodes@gmail.com>
2024-11-27 20:48:58 +0000

    update graal vm to jdk 21

--- .github/scripts/native/graalvm-linux.sh
+++ .github/scripts/native/graalvm-linux.sh
@@ -1,9 +1,9 @@
-curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java11-linux-amd64-22.3.0.tar.gz -o graalvm.tar.gz
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_linux-x64_bin.tar.gz -o graalvm.tar.gz
 mkdir .graalvm
 tar -xf graalvm.tar.gz -C .graalvm
 rm graalvm.tar.gz
-mv .graalvm/graalvm-ce-java11-22.3.0/* .graalvm/
-rm -rf .graalvm/graalvm-ce-java11-22.3.0
+mv .graalvm/graalvm-community-openjdk-*/* .graalvm/
+rm -rf .graalvm/graalvm-community-openjdk-*
 chmod +x .graalvm/bin/gu
 .graalvm/bin/gu install -n native-image
 .graalvm/bin/native-image --version
--- .github/scripts/native/graalvm-win.sh
+++ .github/scripts/native/graalvm-win.sh
@@ -1,7 +1,7 @@
-curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java11-windows-amd64-22.3.0.zip -o graalvm.zip
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_windows-x64_bin.zip -o graalvm.zip
 unzip -q graalvm.zip -d graalvm
 rm graalvm.zip
-mv graalvm/graalvm-ce-java11-22.3.0/* graalvm/
-rm -rf graalvm/graalvm-ce-java11-22.3.0
+mv graalvm/graalvm-community-openjdk-*/* graalvm/
+rm -rf graalvm/graalvm-community-openjdk-*
 graalvm/bin/gu.cmd install -n native-image
 graalvm/bin/native-image.cmd --version
